796de319c8ebc292f623563c0fe5b03148b1057d
CristianAngarita <angarita99@hotmail.com>
2025-07-23 15:27:01 -0500

    Ejercicio appi 2 con response

---
+++ 11-Apis/api-rest/db/database.go
@@ -0,0 +1,79 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+//username:password@tcp(localhost:3306)/database
+
+const url = "root:@tcp(localhost:3306)/goweb_db"
+
+var db *sql.DB
+
+func Connect() {
+	connection, err := sql.Open("mysql", url)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Conexión exitosa")
+	db = connection
+}
+
+func Close() {
+	db.Close()
+}
+
+func Ping() {
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+}
+func ExistTable(tableName string) bool {
+	sql := fmt.Sprintf("SHOW TABLES LIKE '%s'", tableName)
+	row, err := db.Query(sql)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	return row.Next()
+}
+
+func CreateTable(schema string, name string) {
+
+	if !ExistTable(name) {
+		_, err := db.Exec(schema)
+		if err != nil {
+			fmt.Println("Error ", err)
+		}
+	}
+
+}
+
+func TruncateTable(tableName string) {
+	sql := fmt.Sprintf("TRUNCATE %s", tableName)
+	Exec(sql)
+}
+
+func Exec(query string, args ...interface{}) (sql.Result, error) {
+	Connect()
+	result, err := db.Exec(query, args...)
+	Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return result, err
+}
+
+func Query(query string, args ...interface{}) (*sql.Rows, error) {
+	Connect()
+	rows, err := db.Query(query, args...)
+	Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return rows, err
+}
